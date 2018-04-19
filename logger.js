102ea85494ef45373943a578cf7ef1959e6d2b6c
Anastasia <ans.dudarenko@gmail.com>
2018-04-19 11:54:37 +0200

    suka

---
+++ logger.js
@@ -0,0 +1,25 @@
+const winston = require('winston');
+winston.emitErrs = true;
+
+let logger = new winston.Logger({
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            handleExceptions: false,
+            json: false,
+            colorize: true
+        }),
+        new winston.transports.File({
+            filename: 'server.verbose.log',
+            level: 'verbose'
+        })
+    ],
+    exitOnError: false
+});
+
+module.exports = logger;
+module.exports.stream = {
+    write: function(message, encoding){
+        logger.info(message);
+    }
+};
